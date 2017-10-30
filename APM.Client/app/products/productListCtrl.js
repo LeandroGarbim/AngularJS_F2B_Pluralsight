70b698ff6956eee8b3ec0cfc5c4b1b5b28982e82
Leandro Garbim <leandrogarbim@hotmail.com>
2017-10-30 15:11:51 -0200

    adiciona APM.Client

---
+++ APM.Client/app/products/productListCtrl.js
@@ -0,0 +1,20 @@
+(function () {
+    "use strict";
+    angular
+        .module("productManagement")
+        .controller("ProductListCtrl",
+        ["productResource", ProductListCtrl]);
+
+
+    function ProductListCtrl(productResource) {
+       
+        var vm = this;
+
+        vm.searchCriteria = "GDN";
+
+        productResource.query({$filter:"Price ge 5 and Price le 20"},function (data) {
+            vm.products = data;
+        });     
+
+    }
+}());
